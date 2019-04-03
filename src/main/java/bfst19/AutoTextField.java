ac2f62311c8e683dda6481aa5b48498444930c68
Isabell <imat@itu.dk>
2019-04-03 21:55:05 +0200

    "Made the AutoTextField class work"

--- src/main/java/bfst19/AutoTextField.java
+++ src/main/java/bfst19/AutoTextField.java
@@ -1,7 +1,13 @@
 package bfst19;
 
+import javafx.geometry.Side;
 import javafx.scene.control.ContextMenu;
+import javafx.scene.control.CustomMenuItem;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class AutoTextField extends TextField {
 
@@ -27,7 +33,25 @@ public class AutoTextField extends TextField {
     }
 
     private void showResults(){
-        controller.parseSearchText(this.getText());
+
+        addAdressesToDropDown("It works");
+        if(!adressDropDown.isShowing()){
+            adressDropDown.show(AutoTextField.this, Side.BOTTOM,0,0);
+        }
+        //controller.parseSearchText(this.getText());
+    }
+
+    private void addAdressesToDropDown(String adress) {
+
+        List<CustomMenuItem> menuItems = new LinkedList<>();
+
+        Label labelAdress = new Label(adress);
+
+        CustomMenuItem item = new CustomMenuItem(labelAdress, true);
+
+        menuItems.add(item);
+
+        adressDropDown.getItems().addAll(menuItems);
     }
 
 
