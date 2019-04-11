55e8761d4e7c05b7953a1ba3926922e5391070e2
Niva <niva@itu.dk>
2019-04-11 12:22:24 +0200

    Added partially functional rangeQuery tests

--- src/test/java/bfst19/KDTreeTest.java
+++ src/test/java/bfst19/KDTreeTest.java
@@ -1,6 +1,9 @@
 package bfst19;
 
 import bfst19.KDTree.KDTree;
+import javafx.geometry.Point2D;
+import javafx.scene.transform.Affine;
+import javafx.scene.transform.NonInvertibleTransformException;
 import org.junit.Before;
 import org.junit.Test;
 import java.util.*;
@@ -15,12 +18,19 @@ public class KDTreeTest {
 	//LongIndex<OSMWay> idToWay = new LongIndex<OSMWay>();			for relations
 	Map<WayType, List<Drawable>> ways = new EnumMap<>(WayType.class);
 
+	Affine transform = new Affine();
+
+	int servicePolylines;
+
 	@Before
 	public void initialize() {
 		//Fill map with empty arraylists as setup
 		for (WayType type : WayType.values()) {
 			ways.put(type, new ArrayList<>());
 		}
+
+		//Setup transform
+		transform.prependScale(1,-1, 0, 0);
 
 		//Set up a bunch of nodes for putting in ways
 		OSMNode node = new OSMNode(5852189410L, 14.7999881f, 55.2672338f);
@@ -54,13 +64,16 @@ public class KDTreeTest {
 		way.add(idToNode.get(5852189414L));
 		ways.get(type).add(new Polyline(way));
 
+		servicePolylines++;
 
-		way = new OSMWay(619269637); //2 nodes?
+
+		way = new OSMWay(619269637);
 		type = WayType.SERVICE;
 		way.add(idToNode.get(5852189410L));
 		way.add(idToNode.get(5852189412L));
 		ways.get(type).add(new Polyline(way));
 
+		servicePolylines++;
 
 		way = new OSMWay(619270946);
 		type = WayType.SERVICE;
@@ -69,12 +82,15 @@ public class KDTreeTest {
 		way.add(idToNode.get(5852195007L));
 		ways.get(type).add(new Polyline(way));
 
+		servicePolylines++;
 
 		way = new OSMWay(619274513);
 		type = WayType.SERVICE;
 		way.add(idToNode.get(5852227208L));
 		way.add(idToNode.get(5852227209L));
 		ways.get(type).add(new Polyline(way));
+
+		servicePolylines++;
 
 		//Stolen from Model
 		//Make and populate KDTrees for each WayType
@@ -101,5 +117,80 @@ public class KDTreeTest {
 
 		//Tests that root's left and right child are null
 		assertTrue(kdTreeMap.get(WayType.DITCH).getRoot().getNodeL() == null && kdTreeMap.get(WayType.DITCH).getRoot().getNodeR() == null);
+	}
+
+
+	//TODO This fails, root.NodeL has 3 elements, root.NodeR is null and it is supposed to have 4 elements total
+	@Test
+	public void GetAllServiceLines(){
+		//Tests that KDTree for SERVICE WayType has as many elements
+
+		//Actual min and max coords
+		//min X 14.7818229		min Y 55.1828097
+		//max X 14.8497670		max Y 55.2675671
+		Point2D minPoint = getModelCoords(14.6818229f, -55.0828097f);
+		Point2D maxPoint = getModelCoords(14.9497670f, -55.3675671f);
+		//BoundingBox with a BB bigger than the coords to catch everything
+		BoundingBox bb = new BoundingBox(minPoint.getX(), minPoint.getY(),
+				maxPoint.getX()-minPoint.getX(), maxPoint.getY()-minPoint.getY());
+
+		assertEquals(servicePolylines, ((Set<Drawable>) kdTreeMap.get(WayType.SERVICE).rangeQuery(bb)).size());
+	}
+
+
+	@Test
+	public void GetMiddleHalfServiceLines(){
+		//Tests rangeQuery gets the correct middle elements of the KDTree for SERVICE WayType
+
+
+		/*Center  X			Y
+		14.800080			55.267468
+		14.800179			55.267275
+		14.849615			55.243413
+		14.781979			55.182817
+
+		//Actual min and max coords for 2 middle elements
+		//min X 14.7997896		min Y 55.2433121
+		//max X 14.8493998		max Y 55.2673134
+		*/
+		Point2D minPoint = getModelCoords(14.800050f, -55.243313f);
+		Point2D maxPoint = getModelCoords(14.810179f, -55.267375f);
+		//BoundingBox with a BB bigger than the coords to catch everything
+		BoundingBox bb = new BoundingBox(minPoint.getX(), minPoint.getY(),
+				maxPoint.getX()-minPoint.getX(), maxPoint.getY()-minPoint.getY());
+
+		int i = ((Set<Drawable>) kdTreeMap.get(WayType.SERVICE).rangeQuery(bb)).size();
+
+		//TODO Ensure the found elements are the right ones instead of just matching a certain number
+		assertEquals(servicePolylines/2, i);
+	}
+
+
+	//TODO This fails, root.NodeL has 3 elements, root.NodeR is null and it is supposed to have 4 elements total
+	@Test
+	public void CheckEmptyQueryBox(){
+		//Tests that KDTree for SERVICE WayType has as many elements
+
+		//Actual min and max coords
+		//min X 14.7818229		min Y 55.1828097
+		//max X 14.8497670		max Y 55.2675671
+		Point2D minPoint = getModelCoords(10f, -20f);
+		Point2D maxPoint = getModelCoords(11f, -23f);
+		//BoundingBox with a BB bigger than the coords to catch everything
+		BoundingBox bb = new BoundingBox(minPoint.getX(), minPoint.getY(),
+				maxPoint.getX()-minPoint.getX(), maxPoint.getY()-minPoint.getY());
+
+		assertEquals(0, ((Set<Drawable>) kdTreeMap.get(WayType.SERVICE).rangeQuery(bb)).size());
+	}
+
+
+	//Test helper method
+	Point2D getModelCoords(double x, double y) {
+		try{
+			return transform.inverseTransform(x,y);
+		}catch (NonInvertibleTransformException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
